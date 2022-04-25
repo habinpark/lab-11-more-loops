03e0a9c15a0e71e1f5f0c1cc3917b64feedea67c
Habin Park <HabinPark@community.int>
2022-04-25 16:05:39 -0400

    gimmymynumber

---
+++ gimmymynumber.py
@@ -0,0 +1,6 @@
+userNum = int(input("Gimme a number greater than 100...: "))
+
+while userNum<100:
+    print(userNum, "is less than 100, dummy. Try again… I've got all day…”")
+    userNum = int(input("Gimme a number greater than 100...: "))
+print(userNum, "is greater than 100, Good Job!")
